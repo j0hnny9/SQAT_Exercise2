979932721cf408375cdd616bf88059614f6c37a2
somename <someemail>
2016-10-21 14:47:34 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 	
 	@Test
-	public void test_executeCommand() {
+	public void test_executeCommand_Without_obstacles__ffrf__position_1_2() {
 		PlanetExplorer explorer = new PlanetExplorer(3,3,"");
 		String posObst = explorer.executeCommand("ffrf");
 		assertEquals("(1,2)", posObst);
